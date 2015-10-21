b4a6ae43a278930db550fe3dab4a63b04cfb1a2c
Timo Schick <schickt93@gmail.com>
2015-10-21 14:17:08 +0200

    color pixels by region's mean color and major refactoring

--- main.cpp
+++ main.cpp
@@ -1,14 +1,39 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include <queue>
-#include <algorithm>
-#include <vector>
+#include "main.h"
 
-using namespace cv;
-using namespace std;
+// Maximum color distance derived from RGB(0,0,0) and RGB(255,255,255)
+const int MAX_SQUARED_COLOR_DISTANCE = getSquaredColorDistance(Vec3b(0,0,0), Vec3b(255,255,255));
+
+int getSquaredColorDistance(Vec3b c1, Vec3b c2) {
+	return (c1[0]-c2[0])*(c1[0]-c2[0]) + (c1[1]-c2[1])*(c1[1]-c2[1]) + (c1[2]-c2[2])*(c1[2]-c2[2]);
+}
+
+class UsedPixelsMap {
+public: 
+
+	Mat usedPixels;
+
+	UsedPixelsMap() { }
+
+	UsedPixelsMap(int rows, int cols) {
+		usedPixels = Mat(rows, cols, CV_8UC1, cv::Scalar(255));
+	}
+
+	// returns true if p has already been added to a region
+	bool isUsed(Point p) {
+		return usedPixels.at<uchar>(p) != 255;
+	}
+
+	// marks p as used
+	void setUsed(Point p) {
+		usedPixels.at<uchar>(p) = 0;
+	}
+
+	// marks all pixels in usedPixelsMap as unused
+	void reset() {
+		usedPixels = Scalar(255);
+	}
+
+};
 
 struct PointWithGradient {
 	Point pos;
@@ -16,219 +41,159 @@ struct PointWithGradient {
 };
 
 class ComparePWG {
-    public:
+public:
 
 	// Returns true if t1 has a higher gradient than t2
-    bool operator()(PointWithGradient& t1, PointWithGradient& t2) {  
-       if (t1.gradient > t2.gradient) return true;
-       return false;
-    }
+	bool operator()(PointWithGradient& t1, PointWithGradient& t2) {  
+		return (t1.gradient > t2.gradient);
+	}
 };
 
 priority_queue<PointWithGradient, vector<PointWithGradient>, ComparePWG> pq, backupQueue;
-Mat usedPixels;
-Mat mask;
+UsedPixelsMap usedPixels;
 int edgeThresh = 1;
 Mat original;
 
 // define a trackbar callback
-static void onTrackbar(int, void*)
-{
+static void onTrackbar(int, void*) {
+
 	Mat im = original.clone();
-	Mat up = usedPixels.clone();
+	usedPixels.reset();
 
-  mask = Scalar::all(0);
-  int i=0;
-  while (!pq.empty()) {
-	  
-       PointWithGradient t2 = pq.top();
-	   
-	  if(mask.at<uchar>(t2.pos.x-1, t2.pos.y-1) == 0) {
-		  i++;
-		  Vec3b c1 = im.at<Vec3b>(t2.pos);
-		Scalar newVal = Scalar(c1[0], c1[1], c1[2]);
-		   int lo = edgeThresh/2;
-		   int up = edgeThresh/2;
-		   int flags = 4 + (255 << 8); // + FLOODFILL_MASK_ONLY;
-		   //Rect ccomp;
+	edgeThresh = edgeThresh * MAX_SQUARED_COLOR_DISTANCE / 100;
 
-		   floodFill(im, mask, t2.pos, newVal, 0, Scalar(lo, lo, lo), Scalar(up, up, up), flags);
-		   
-	  }
+	//int squaredEdgeThresh = edgeThresh * edgeThresh;
 
+	while (!pq.empty()) {
 
+		PointWithGradient t2 = pq.top();
+		backupQueue.push(t2);
+		pq.pop();
 
-	   /*
-	   // if the pixel at position t2 has not been used before
-	   if(up.at<uchar>(t2.pos) == 255) {
+		// if the pixel at position t2 has not been used before
+		if(usedPixels.isUsed(t2.pos)) {
+			continue;
+		}
 
-		   // store the current pixel's color
-		   Vec3b c1 = im.at<Vec3b>(t2.pos);
+		// store the current pixel's color
+		Vec3b c1 = im.at<Vec3b>(t2.pos);
 
-		   // create a region for the current pixel
-			vector<Point> region;
-			region.push_back(t2.pos);
+		// create a region for the current pixel
+		vector<Point> region;
+		// stores the colors of all points belonging to the region
+		vector<Vec3b> regionColors;
+		stack<Point> stack;
+		region.push_back(t2.pos);
+		regionColors.push_back(c1);
+		stack.push(t2.pos);
 
-		   // mark pixel as used
-		   up.at<uchar>(t2.pos) = 0;
+		// mark pixel as used
+		usedPixels.setUsed(t2.pos);
 
-		   // iterate over all 4-connected pixels
-		   for(int i=-1; i<=1; i+=2) {
-			for(int j=-1; j<=1; j+=2) {
+		while(!stack.empty()) {
+			Point p = stack.top();
+			stack.pop();
 
-				// Existiert der Punkt ¸berhaupt? Pixel am Rand haben ja nur 3 bzw. an den Ecken 2 4-connected Nachbarn
-				if(t2.pos.x + i >= 0 && t2.pos.x + i < im.cols && t2.pos.y + j >= 0 && t2.pos.y + j < im.rows) { 
-					Point p = Point(t2.pos.x + i, t2.pos.y + j);
-					if(up.at<uchar>(p) == 255) {
-						// pixel not yet used
-						// color distance zwischen dem Punkt aus der Priority Queue (t2) und dem aktuellen Nachbarn (p) berechnen
-						Vec3b c2 = im.at<Vec3b>(p);
-						double colorDistance = sqrt((c1[0]-c2[0])*(c1[0]-c2[0]) + (c1[1]-c2[1])*(c1[1]-c2[1]) + (c1[2]-c2[2])*(c1[2]-c2[2]));
-						if(colorDistance < edgeThresh) {
-							region.push_back(p);
-							// TODO: diese 2 Zeilen entfernen
-							im.at<Vec3b>(p) = im.at<Vec3b>(t2.pos);
-							up.at<uchar>(p) = 0;
+			// iterate over all 4-connected pixels
+			for(int i=-1; i<=1; i+=2) {
+				for(int j=-1; j<=1; j+=2) {
+
+					bool pointExists = p.x + i >= 0 && p.x + i < im.cols && p.y + j >= 0 && p.y + j < im.rows;
+
+					if(pointExists) { 
+						Point p2 = Point(p.x + i, p.y + j);
+						if(!usedPixels.isUsed(p2)) {
+							// pixel not yet used
+							// color distance zwischen dem Punkt aus der Priority Queue (t2) und dem aktuellen Nachbarn (p) berechnen
+							Vec3b c2 = im.at<Vec3b>(p2);
+							if(getSquaredColorDistance(c1,c2) <= edgeThresh) {
+								region.push_back(p2);
+								regionColors.push_back(c2);
+								usedPixels.setUsed(p2);
+								stack.push(p2);
+							}
 						}
 					}
 				}
 			}
-		   }
+		}
 
-		   // TODO: ¸ber region iterieren, Durchschnittsfarbe berechnen, und jeden Pixel mit dieser Farbe einf‰rben!
+		// calculate mean color of region
+		Scalar mc = (mean(regionColors));
+		Vec3b meanColor = Vec3b(mc[0], mc[1], mc[2]);
 
-	   }*/
+		// color all points in region with corresponding meanColor
+		for(std::vector<Point>::iterator it = region.begin(); it != region.end(); ++it) {
+			im.at<Vec3b>(*it) = meanColor;
+		}
 
-	  
-	   backupQueue.push(t2);
-       pq.pop();
-    }
-
-  
-	  cout << i << endl;
+	}
 
 	pq.swap(backupQueue);
-    cv::imshow("Edge map", im);
-	cv::imshow("Mask", mask);
+	imshow("Result", im);
 }
 
-int main( int argc, char** argv )
-{
+int main( int argc, char** argv ) {
 
-  Mat src, src_gray;
-  Mat grad;
-  int scale = 1;
-  int delta = 0;
-  int ddepth = CV_16S;
+	Mat src, src_gray;
+	Mat grad;
+	int scale = 1;
+	int delta = 0;
+	int ddepth = CV_16S;
 
-  int c;
+	int c;
 
-  /// Load an image
-  src = imread( "C:/Users/Timo/Desktop/test.jpg", 1 );
-  original = src.clone();
+	/// Load an image
+	src = imread( argv[1], 1 );
+	original = src.clone();
 
-  if( !src.data )
-  { return -1; }
+	if( !src.data )
+	{ return -1; }
 
-  GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
+	GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
 
-  /// Convert it to gray
-  cvtColor( src, src_gray, CV_RGB2GRAY );
+	/// Convert it to gray
+	cvtColor( src, src_gray, CV_RGB2GRAY );
 
-  /// Generate grad_x and grad_y
-  Mat grad_x, grad_y;
-  Mat abs_grad_x, abs_grad_y;
+	/// Generate grad_x and grad_y
+	Mat grad_x, grad_y;
+	Mat abs_grad_x, abs_grad_y;
 
-  /// Gradient X
-  Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( grad_x, abs_grad_x );
+	/// Gradient X
+	Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+	convertScaleAbs( grad_x, abs_grad_x );
 
-  /// Gradient Y
-  Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( grad_y, abs_grad_y );
+	/// Gradient Y
+	Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+	convertScaleAbs( grad_y, abs_grad_y );
 
-  /// Total Gradient (approximate)
-  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+	/// Total Gradient (approximate)
+	addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
 
-  usedPixels = grad.clone(); 
+	usedPixels = UsedPixelsMap(src.rows, src.cols);
 
-  mask.create(src.rows+2, src.cols+2, CV_8UC1);
-
-  // ‹ber alle Pixel iterieren, um sie in die PriorityQueue einzusortieren
-  for(int y=0;y<grad.rows;y++)
-	{
-		for(int x=0;x<grad.cols;x++)
-		{
-			// Gradient f¸r jeden Pixel auslesen
+	// iterate over all pixels and add them to the priority queue
+	for(int y=0;y<grad.rows;y++) {
+		for(int x=0;x<grad.cols;x++) {
+			// get the gradient for each pixel
 			int gradient = grad.at<uchar>(Point(x,y));
-
-			// usedPixels wird als weiﬂes Bild initialisiert, hier wird sp‰ter gespeichert, welche Pixel beim Auslesen der Priority Queue schon verwendet wurden
-			usedPixels.at<uchar>(Point(x,y)) = 255;
 
 			PointWithGradient pwg = PointWithGradient();
 			pwg.gradient = gradient;
 			pwg.pos = Point(x,y);
 
-			// Store PWG in PriorityQueue
+			// store pwg in priority queue
 			pq.push(pwg);
-		
 		}
 	}
 
-    // create a toolbar
- 
-  namedWindow("Edge map", 1);
-  createTrackbar("threshold", "Edge map", &edgeThresh, 20, onTrackbar);
-  onTrackbar(0, 0);
+	// create a toolbar
+	namedWindow("Result", WINDOW_AUTOSIZE);
+	resizeWindow("Result", original.size().width,original.size().height);
+	createTrackbar("threshold", "Result", &edgeThresh, 100, onTrackbar);
+	onTrackbar(0, 0);
+	imshow("Original", original);
+	waitKey(0);
 
-  cv::imshow("original", original);
-  waitKey(0);
-
-  return 0;
-  }
-
-/*
-Mat image0, image, gray, mask;
-int ffillMode = 1;
-int loDiff = 20, upDiff = 20;
-int connectivity = 4;
-int isColor = true;
-bool useMask = false;
-int newMaskVal = 255;
-
-static void onMouse( int event, int x, int y, int, void* )
-{
-    if( event != EVENT_LBUTTONDOWN )
-        return;
-
-    Point seed = Point(x,y);
-    int lo = ffillMode == 0 ? 0 : loDiff;
-    int up = ffillMode == 0 ? 0 : upDiff;
-    int flags = connectivity + (newMaskVal << 8) +
-                (ffillMode == 1 ? FLOODFILL_FIXED_RANGE : 0);
-    int b = (unsigned)theRNG() & 255;
-    int g = (unsigned)theRNG() & 255;
-    int r = (unsigned)theRNG() & 255;
-    Rect ccomp;
-
-    Scalar newVal = isColor ? Scalar(b, g, r) : Scalar(r*0.299 + g*0.587 + b*0.114);
-    Mat dst = isColor ? image : gray;
-    int area;
-
-    if( useMask )
-    {
-        threshold(mask, mask, 1, 128, THRESH_BINARY);
-        area = floodFill(dst, mask, seed, newVal, &ccomp, Scalar(lo, lo, lo),
-                  Scalar(up, up, up), flags);
-        imshow( "mask", mask );
-    }
-    else
-    {
-        area = floodFill(dst, seed, newVal, &ccomp, Scalar(lo, lo, lo),
-                  Scalar(up, up, up), flags);
-    }
-
-    imshow("image", dst);
-    cout << area << " pixels were repainted\n";
+	return 0;
 }
-*/
---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef main_h
+#define main_h
+ 
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+
+using namespace cv;
+using namespace std;
+
+int getSquaredColorDistance(Vec3b c1, Vec3b c2); 
+ 
+#endif
